ae130c88d8fdd7c0db7ddcaeec130ef9309f396e
Mrudul Bhatt <mrudul1203bhatt@gmail.com>
2022-12-01 17:35:29 +0530

    Add project files.

---
+++ ContactsManager.ControllerTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ContactsManager.ControllerTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ ContactsManager.UI/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("Error")]
+        public IActionResult Error()
+        {
+
+            IExceptionHandlerPathFeature? exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            if (exceptionHandlerPathFeature != null && exceptionHandlerPathFeature.Error != null)
+            {
+                ViewBag.ErrorMessage = exceptionHandlerPathFeature.Error.Message;
+            }
+
+            //Views/Shared/Error.cshtml
+            return View();
+        }
+    }
+}
